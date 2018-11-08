88585550371efeb8a54a405bd4335e63e55f0b54
Eric Zheng <404@example.com>
2018-11-08 17:54:57 +0800

    fix db/migrate for mysql 5.6

--- db/migrate/20180704211100_adjust_field_length.rb
+++ db/migrate/20180704211100_adjust_field_length.rb
@@ -1,10 +1,13 @@
 class AdjustFieldLength < CamaManager.migration_class
   def change
-    post_table = CamaleonCms::Post.table_name
-    change_column post_table, :title, :text
-    remove_index(post_table, :slug) if index_exists?(post_table, :slug)
-    change_column post_table, :slug, :text
-    add_index post_table, :slug, length: 255
-    change_column "#{PluginRoutes.static_system_info["db_prefix"]}term_taxonomy", :name, :text
+    db_prefix = PluginRoutes.static_system_info["db_prefix"]
+
+    change_column "#{db_prefix}posts", :title, :text
+    if index_exists? "#{db_prefix}posts", :slug
+      remove_index "#{db_prefix}posts", :slug
+    end
+    change_column "#{db_prefix}posts", :slug, :text
+    add_index "#{db_prefix}posts", :slug, length: 250
+    change_column "#{db_prefix}term_taxonomy", :name, :text
   end
 end
